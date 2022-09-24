c5cb3ffaaa98cc0ab9a7070fc340ed9aaa6a5a46
Mateus <correia.mateus@hotmail.com>
2022-09-24 10:56:04 -0300

    Tap screen

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,4 +5,17 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
     [SerializeField] private TMP_Text _coinsText;
+    private double _coins;
+    [SerializeField] private TMP_Text _coinsPerSecondsText;
+    private double _coinsPerSeconds;
+
+    private void Start() {
+        _coins = 0;
+        _coinsText.text = _coins.ToString();
+    }
+
+    public void TapScreenButton() {
+        // Damage on enemy
+        Debug.Log("Tap..");
+    }
 }
